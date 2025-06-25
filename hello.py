7609ce5a7364ca25cdba2d8b77cbae9b688da6ac
Jose David <lujan9495@gmail.com>
2025-06-25 09:37:18 -0500

    Enhance hello.py with new endpoints: add home route, update greet to accept name from headers, and introduce get_agent to return user agent. Comment out previous greet implementations for reference.

--- hello.py
+++ hello.py
@@ -1,11 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Header
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/greet")
-async def greet(): 
-  return {"message": "Hello World"}
+@app.get("/")
+def home():
+  return "Welcome to Home"
+
+# @app.get("/greet/{name}")
+# def greet(name: str):
+#   return f"Hello {name}"
+
+# With query param ⬇️
+# @app.get("/greet")
+# def greet(name: str): 
+#   return f"Hello {name}?"
+
+# @app.post("/greet")
+# def greet(name: str = Body(embed=True)): # ⬅️ Body(embed=True) is needed to tell FastAPI that, this time, we get the value of who from the JSON-formatted request body. 
+#   return f"Hello {name}?"
+
+@app.post("/greet")
+def greet(name:str = Header()):
+  return f"Hello {name}?"
+
+@app.post("/agent")
+def get_agent(user_agent: str = Header()): 
+  return user_agent
 
 if __name__ == "__main__": 
   uvicorn.run("hello:app", reload=True)
